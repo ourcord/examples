61b6877f113d32a1e70a5f4cc646712687ae97fb
Misly <60405462+Misly16@users.noreply.github.com>
2020-12-13 17:26:35 +0000

    Create index.ts

---
+++ ts/index.ts
@@ -0,0 +1,18 @@
+import Client from 'ourcord';
+const client = new Client(' token ', {status: 'dnd'});
+client.connect();
+
+client.on('ready', () => {
+  console.log('Client connected to discord API');
+});
+
+client.on('debug', (log) => {
+  console.log(log);
+});
+
+client.on('message', async (msg: any) => {
+  if (msg.author.bot) return;
+  if (msg.content == 'ping') {
+    msg.channel.send('pong')
+  }
+});
